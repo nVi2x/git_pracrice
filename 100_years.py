aad74553f40293fbe970b05adb652a5798f3f59b
Nishit <itsa1v9@gmail.com>
2019-06-11 14:31:00 -0400

    made changes

--- 100_years.py
+++ 100_years.py
@@ -1,4 +1,4 @@
 name = input('Enter your Name: ')
 age = input('Enter your Age: ')
-year_at_100 = (2019 - age) + 100
-print('Alright' + name + 'your age will be 100 years in the year, ' + year_at_100 + '.')
+lol = (2019 - age) + 100
+print('Alright' +name+ 'your age will be 100 years in the year ' +lol+ '.')
