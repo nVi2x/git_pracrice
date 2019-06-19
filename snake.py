dd9bb1f57989790da3616d635215ea2219f4641b
Nishit <itsa1v9@gmail.com>
2019-06-19 18:08:26 -0400

    complete

--- snake.py
+++ snake.py
@@ -27,5 +27,49 @@ class App:
         self.player = player()
     def on_init(self):
         pygame.init()
-        self._display_surf = pygame.display.set_mode((self.windowWidth, self.windowHeight),
-        pygame.display.set_caption('Sup guys dis is a simple snek gaem'))
+        sself._display_surf = pygame.display.set_mode((self.windowWidth,self.windowHeight), pygame.HWSURFACE)
+        
+        pygame.display.set_caption('Sup guys dis is an anguey snek')
+        self._running = True
+        self._image_surf = pygame.image.load("pygame.png").convert()
+    def on_event(self, event):
+        if event.type == quit:
+            self._running = False
+    def on_loop(self):
+        pass
+    def on_render(self):
+        self._display_surf.fill((0,0,0))
+        self._display_surf.blit(self._image_surf, (self.player.x, self.player.y))
+        pygame.display.flip
+    def on_cleanup(self):
+        pygame.quit()
+    def on_execute(self):
+        if self.on_init() == False:
+            self._running = False
+ 
+        while( self._running ):
+            pygame.event.pump()
+            keys = pygame.key.get_pressed() 
+            
+            if (keys[K_RIGHT]):
+                self.player.moveRight()
+
+            if (keys[K_LEFT]):
+                self.player.moveLeft()
+
+            if (keys[K_UP]):
+                self.player.moveUp()
+
+            if (keys[K_DOWN]):
+                self.player.moveDown()
+
+            if (keys[K_ESCAPE]):
+                self._running = False
+
+            self.on_loop()
+            self.on_render()
+        self.on_cleanup()
+ 
+if __name__ == "Snek" :
+    theApp = App()
+    theApp.on_execute()
