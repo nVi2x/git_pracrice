24af0491c7218886d890674b89c5b902f51cd581
Nishit <itsa1v9@gmail.com>
2019-06-25 11:02:08 -0400

    doen with 8

--- 8.largest_of_3_nums.cpp
+++ 8.largest_of_3_nums.cpp
@@ -8,16 +8,12 @@ int main() {
     cin >> n1 >> n2 >> n3;
 
     if(n1 >= n2 && n1 >= n3)
-    {
         cout << "Largest number is: " << n1;
-    }
-    if(n2 >= n1 && n2 >= n3)
-    {
+    
+    else if(n2 >= n1 && n2 >= n3)
         cout << "Largest number is: " << n2;
-    }
-    if(n3 >= n1 && n3 >= n2)
-    {
+    
+    else
         cout << "Largest number is: " << n3;
-    }
     return 0;
 }
