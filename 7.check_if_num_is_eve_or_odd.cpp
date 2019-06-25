1c4935eda9affa3ddc6091d2b781932e869502fc
Nishit <itsa1v9@gmail.com>
2019-06-25 00:55:56 -0400

    7 complete

---
+++ 7.check_if_num_is_eve_or_odd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter an integer: ";
+    cin >> n;
+
+    if (n % 2 == 0)
+        cout << "The number is Even.";
+    else
+        cout << "The number is Odd.";
+
+    return 0;
+}
