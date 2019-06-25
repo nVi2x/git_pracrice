fc3c71cc971b62937c70fa55afc411d50a858a59
Nishit <itsa1v9@gmail.com>
2019-06-24 21:36:46 -0400

    output number

---
+++ output_a_number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int number;
+    cout << "dude enter an integer and stop being soo dramatic: ";
+    cin >> number;
+
+    cout << "k bruh u enterd " << number;
+
+}
